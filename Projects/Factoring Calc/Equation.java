fa978359c25c6640d8bd78114b8a09e3f7d0a204
bglossner <benglossner@gmail.com>
2019-09-02 12:45:25 -0700

    removing pics and videos and projects

--- Projects/Factoring Calc/Equation.java
+++
@@ -1,7 +0,0 @@
-public class Equation
-{
-    public Equation(String str)
-    {
-        BPad.eqs.add(new Equation(str));
-    }
-}
--- Projects/Python Projects/Calculator/Test files/statsTester.py
+++
@@ -1,7 +0,0 @@
-import stats_calc
-import sys
-##stats_calc.makeList()
-##print(stats_calc.listDic)
-sys.stdout.write("One")
-sys.stdout.flush()
-sys.stdout.write("Two")
